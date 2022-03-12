d4d6b50a816035830289b1517f72770c5eab5179
Arthur Ramires <arthur.bueno@before.com.br>
2022-03-12 09:53:31 -0400

    Creating hooks to handle products in Cart

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
+      <CartProvider>
         <GlobalStyles />
         <Header />
         <Routes />
         <ToastContainer autoClose={3000} />
+      </CartProvider>
     </BrowserRouter>
   );
 };
